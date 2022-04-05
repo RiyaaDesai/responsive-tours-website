f99c4325095df747f8215f605cc61ecef3781fc1
RiyaaDesai <riyadesai1804@gmail.com>
2022-04-05 13:37:30 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    });  
+  });
+
+$(document).ready(function() {
+    $('#p-autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#p-autoWidth').removeClass('p-cS-hidden');
+        } 
+    });  
+  });
+
+$(document).ready(function() {
+    $('#r-autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#r-autoWidth').removeClass('r-cS-hidden');
+        } 
+    });  
+  });
+
